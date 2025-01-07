8f5b4c31e05d1ee4f49646bab2953f7cf0920e46
kad5 <khaled.mohamed.1807@gmail.com>
2025-01-07 22:54:24 +0200

    changed the api to a better one. added some helper functions. now i can fetch data for 2 days ahead and 2 days back with cities name search that would be on input

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,88 @@
-const mykey = "b4a5b744174e43287529d8fbbcf7bd53";
-const units = [{ unit: "imperial" }, { unit: "metric" }];
-let unitChoice = units[1].unit;
+const apiKey = "f14c6672ce5c4e0da53185847250701";
+const daysArry = getDays();
 
-const locationsList = async (searchTerm) => {
-  const openWeatherJson = await fetch(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=5&appid=${mykey}`,
-    { mode: "cors" }
-  );
-  const locationsList = await openWeatherJson.json();
-  console.log(locationsList);
-  locationWeather(locationsList[0].lat, locationsList[0].lon);
-};
+// takes a string from the input and returns an array of similar city names
+async function searchLocationsByName(cityString) {
+  let citiesArray = [];
+  try {
+    const fetchObj = await fetch(
+      `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${cityString}`
+    );
+    citiesArray = await fetchObj.json();
+  } catch (error) {
+    citiesArray = [];
+  }
 
-const locationWeather = async (lat, lon) => {
-  const openWeatherJson = await fetch(
-    `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${mykey}&units=${unitChoice}`
-  );
-  const locationWeather = await openWeatherJson.json();
-  console.log(locationWeather);
-};
-locationsList("cairo");
+  if (citiesArray && citiesArray.length > 0) {
+    citiesArray.forEach((city) => {
+      extractCityData(city);
+    });
+  } else null;
+}
+
+// constructs the cities list on input seach  and passes latitude and longitude to the api
+function extractCityData(city) {
+  const fullName = `${city.name}, ${city.country}`;
+  const lat = city.lat;
+  const lon = city.lat;
+  console.log(fullName);
+}
+
+//gets yesterday, and day before yesterday dates in iso format to be passed into the api
+function getDays() {
+  const today = new Date();
+  return [
+    new Date(today.setDate(today.getDate() - 1)).toISOString().split("T")[0],
+    new Date(today.setDate(today.getDate() - 2)).toISOString().split("T")[0],
+  ];
+}
+
+// helper that takes a fetch url and fetches the weather object from the api
+async function fetchWeatherData(url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch: ${response.statusText}`);
+  }
+  return response.json();
+}
+
+// calls the api to fetch data for 5 days, gets the date from getDays and runs it to fetchWeatherData
+async function getLocationWeather(lat, lon) {
+  try {
+    const [
+      {
+        forecast: {
+          forecastday: [dayN2W],
+        },
+      },
+      {
+        forecast: {
+          forecastday: [dayN1W],
+        },
+      },
+      {
+        forecast: {
+          forecastday: [todayW, dayP1W, dayP2W],
+        },
+      },
+    ] = await Promise.all([
+      fetchWeatherData(
+        `https://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${lat},${lon}&dt=${daysArry[1]}`
+      ),
+      fetchWeatherData(
+        `https://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${lat},${lon}&dt=${daysArry[0]}`
+      ),
+      fetchWeatherData(
+        `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lon}&days=3&aqi=no&alerts=no`
+      ),
+    ]);
+
+    return [dayN2W, dayN1W, todayW, dayP1W, dayP2W];
+  } catch (error) {
+    console.error("Error fetching or processing weather data:", error);
+    return null;
+  }
+}
+
+searchLocationsByName("alex");
+getLocationWeather("30.0444", "31.2357");
